b209d3416e88eb7755d51bf6af89fcd547cc2a26
thhoang <37319983+thhoang02@users.noreply.github.com>
2024-02-13 20:52:14 -0800

    loop to list name and height of pokemon
    loop to write name, height, and comment on height if greater than 1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,32 @@
+// array of name, height, and types of different pokemon
 let pokemonList = [
     {
         name: 'Bulbasaur',
-        height: 7,
+        height: 0.7,
         types: ['grass', 'poison']
     },
     {
         name: 'Charmander',
-        height: 6,
+        height: 0.6,
         types: ['fire']
     },
     {
+        name: 'Charizard',
+        height: 1.7,
+        types: ['fire', 'flying']
+    },
+    {
         name: 'Squirtle',
-        height: 5,
+        height: 0.5,
         types: ['water']
     }
 ];
+
+// loop to write name, height, and comment on height if greater than 1.5
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+    if (pokemonList[i].height > 1.5) {
+        document.write(" - wow, thats big!");
+    };
+    document.write("<br>");
+};
