ed348e5474d2f0f32f8902bf89d5fe981170c47a
thhoang <37319983+thhoang02@users.noreply.github.com>
2024-02-14 22:38:07 -0800

    IIFE added to pokemonList
    added feature to add(), getAll(). new pokemon added to the pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,58 @@
-// array of name, height, and types of different pokemon
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Charmander',
-        height: 0.6,
-        types: ['fire']
-    },
-    {
-        name: 'Charizard',
-        height: 1.7,
-        types: ['fire', 'flying']
-    },
-    {
-        name: 'Squirtle',
-        height: 0.5,
-        types: ['water']
-    }
-];
+// returns add pokemon or getAll pokemon. name, height(meter), types (element)
+let pokemonRepository = (function () {
 
-// function to write name, height, and comment on height if greater than 1.5
-function loopPokemon(pokemon) {
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Charmander',
+            height: 0.6,
+            types: ['fire']
+        },
+        {
+            name: 'Charizard',
+            height: 1.7,
+            types: ['fire', 'flying']
+        },
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            types: ['water']
+        }
+    ];
+
+    // adds new pokemon to array
+    function add(newPokemon) {
+        pokemonList.push(newPokemon);
+    }
+
+    // return array of pokemon
+    function getAll() {
+        return pokemonList;
+    }
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+// new pokemon added
+pokemonRepository.add(
+    {
+        name: 'Ekans',
+        height: 2.0,
+        types: ['poison']
+    }
+);
+
+// prints pokemon from pokemonRepository with name and height detail
+pokemonRepository.getAll().forEach(function (pokemon) {
     document.write(pokemon.name + " (height: " + pokemon.height + ")");
     if (pokemon.height > 1.5) {
         document.write(" - wow, thats big!");
     };
     document.write("<br>");
-};
-
-pokemonList.forEach(loopPokemon);
+}); 
